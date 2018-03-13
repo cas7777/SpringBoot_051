7a89dc03f55e07b75fef39996b2e7e8c0718514c
GBTC441008UR <GBTC441008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-13 10:45:48 -0400

    Project 5

---
+++ src/main/java/com/example/demo/SpringBoot051Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot051Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot051Application.class, args);
+	}
+}
